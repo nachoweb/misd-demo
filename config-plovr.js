bce7a889b17141eb5fcb854c6c2a1dbca8bd0320
Nachoweb <nachoelg@gmail.com>
2012-05-15 18:35:21 +0200

    first commit

---
+++ config-plovr.js
@@ -0,0 +1,19 @@
+{
+  // The id is used as a query parameter in the src of the <script> tag.
+  "id": "misd",
+  "paths": ".",
+  //Bootstrap de la aplicación
+  "inputs": "js/app.js",
+  //En produccion pondremos mode ADVANCED
+  "mode":"SIMPLE",
+  "level":"VERBOSE",
+  "css-inputs":"gss/style.gss",
+  "css-output-file":"gss/build/buildcss.css",
+	"output-file":"js/build/buildapp.js",
+	"externs":"externs.js",
+	"define":{
+		//Lo pondremos a false cuando queramos generar el archivo de producción para quitar todo el codigo que vaya en un if(goog.DEBUG)
+		"goog.DEBUG":true  
+	}
+  
+}
---
+++ js/app.js
@@ -0,0 +1,19 @@
+goog.provide('mbf.bootstrap');
+
+goog.require('mbf.test.tmpl');
+
+
+/**
+ * Funcion que inicializa la aplicación.
+ * Será llamada en el evento onload
+ */
+mbf.bootstrap=function(){
+
+	if(goog.DEBUG){
+		console.log("Aplicación arrancada");
+		document.getElementById("body").innerHTML=mbf.test.tmpl.test({testnum:4});
+	}
+
+};
+
+goog.exportSymbol('mbf.bootstrap', mbf.bootstrap);
